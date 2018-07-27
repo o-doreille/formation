48d89cd40ef1319bd207c178af30d125ce8390d6
odoreille <olivier.doreille@syntheses.fr>
2018-07-27 12:19:32 +0200

    modif du hello controller

---
+++ src/Controller/helloController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Olivier Doreille
+ * Date: 27/07/2018
+ * Time: 12:14
+ */
+
+namespace App\Controller;
+
+
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/")
+ */
+
+class helloController
+{
+    /**
+     * @Route("/hello")
+     */
+    public function hello(){
+
+
+    }
+}
